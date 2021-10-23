3f1ff552278e9e39ba9d3a8d35c89d15485f6347
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-10-23 12:00:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route,Switch } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Switch>
+            <Route exact path="/">
+              <ProductList />
+            </Route>
+            <Route exact path="/add">
+              <AddProduct />
+            </Route>
+            <Route exact path="/edit/:id">
+              <EditProduct />
+            </Route>
+          </Switch>
+        </div>
+      </div>
     </div>
+    </Router>
   );
 }
 
